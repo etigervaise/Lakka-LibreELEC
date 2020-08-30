41c49448c6d7d4c885595f061e1946bdd8ba66af
Your Name <you@example.com>
2020-08-30 11:56:09 +0000

    Update kernel 5.4.61

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -44,8 +44,7 @@ case "$LINUX" in
     PKG_PATCH_DIRS="default"
     ;;
   *)
-    PKG_VERSION="5.1.18"
-    PKG_SHA256="6013e7dcf59d7c1b168d8edce3dbd61ce340ff289541f920dbd0958bef98f36a"
+    PKG_VERSION="5.4.61"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v5.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
